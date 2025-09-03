0bf46652d896d9f59273133d5103d1194e960cd1
karisma-dana <karisma.dana2007@gmail.com>
2025-09-03 23:12:11 +0800

    just try something diffrent

--- basic/main.py
+++ basic/main.py
@@ -2,8 +2,14 @@ from bs4 import BeautifulSoup
 
 
 with open("basic/home.html", "r") as html_file:
-    content = html_file.read()
-    soup = BeautifulSoup(content, "lxml")
-    tags = soup.find_all("h5")
-    for tag in tags:
-        print(tag)
+    content = html_file.read()  # read the content of the file
+    soup = BeautifulSoup(
+        content, "lxml"
+    )  # parse the content of the file using BeautifulSoup and lxml parser
+    courses = soup.find_all(
+        "div", class_="card"
+    )  # find all h5 tags on the page, and retuurn as a list
+    for course in courses:
+        course_name = course.h5.text
+        course_price = course.a.text.split()[-1]
+        print(f"{course_name} costs {course_price}")
