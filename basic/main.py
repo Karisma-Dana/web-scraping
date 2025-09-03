d6faec49949a7722fce63e1371dbc94cb00dab0f
karisma-dana <karisma.dana2007@gmail.com>
2025-09-03 14:54:29 +0800

    clear learn the basic scraping

---
+++ basic/main.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+
+
+with open("basic/home.html", "r") as html_file:
+    content = html_file.read()
+    soup = BeautifulSoup(content, "lxml")
+    tags = soup.find_all("h5")
+    for tag in tags:
+        print(tag)
